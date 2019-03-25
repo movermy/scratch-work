2b29f0d3fa8350a37dfbabf4c6054a8f4f781031
MarkOvermyer <movermye@its.jnj.com>
2019-03-24 22:02:14 -0400

    Budget: clean up code formatting and labeling, as well as plot labeling. No functional change.

--- budget/analyze_spend.py
+++ budget/analyze_spend.py
@@ -8,7 +8,11 @@ Created on Fri Mar  8 16:21:03 2019
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+import datetime as dt
+from sklearn.linear_model import LinearRegression
+import numpy as np
 plt.close('all')
+
 
 def chase_csv_import(path):
     # When pointed to a directory of chase bank csv export, this 
@@ -43,6 +47,8 @@ card_df['Date'] =pd.to_datetime(card_df['Post Date'])
 checking_df = chase_csv_import("checking-data")
 checking_df['source'] = 'checking'
 checking_df['Date'] =pd.to_datetime(checking_df['Posting Date'])
+checking_df['epoch'] = (checking_df['Date'] - dt.datetime(1970,1,1)).dt.total_seconds()
+checking_df['days'] = (checking_df['epoch'] - checking_df['epoch'].min())/(3600 * 24)
 checking_df.sort_values(by=['Date'], inplace=True)
 checking_df['test_balance'] = checking_df.Amount.cumsum() + checking_df.Balance.values[0] + 2300
 select_crit = ((~checking_df.Description.str.contains('J&J')) &\
@@ -73,16 +79,31 @@ itunes_spend = df.loc[crit].Amount.sum()
 crit = (df['no_num_description'].str.lower().str.contains('j&j'))
 mark = df.loc[crit]
 
-crit = df.source.str.contains("checking")
-plt.plot(df.loc[crit].Date, df.loc[crit].Balance, 'r*')
-crit = df.source.str.contains("savings")
-plt.plot(df.loc[crit].Date, df.loc[crit].Balance, 'g*')
-
+# plot raw balences 
 f,a = plt.subplots(1,1)
-a.plot(checking_df.Date, checking_df.Balance, label='Balance')
-a.plot(checking_df.Date, checking_df.select_balance, label='Select Balance')
-a.plot(checking_df.Date, checking_df.test_balance, label='Test Balance')
+f.suptitle('Summary of Chase accounts - raw')
+crit = df.source.str.contains("checking")
+a.plot(df.loc[crit].Date, df.loc[crit].Balance, label='Checking Balance')
+crit = df.source.str.contains("savings")
+a.plot(df.loc[crit].Date, df.loc[crit].Balance, label='Savings Balance')
 a.legend()
 
+# create "spend/day" trendline
+start_time = '2017-05-18' #after this day, data is clean
+checking_df.dropna(subset=['epoch', 'select_balance'], inplace=True)
+X = checking_df.loc[checking_df.Date > start_time].days
+Y = checking_df.loc[checking_df.Date > start_time].select_balance
+z = np.polyfit(X, Y, 1)
+p = np.poly1d(z)
 
+# plot balance with fit line
+f,a = plt.subplots(1,1)
+f.suptitle(f'Spend rate is ${abs(round(p[1],1))}/day')
+a.plot(checking_df.days, checking_df.Balance, '+', label='Balance')
+a.plot(checking_df.days, checking_df.select_balance, '-', label='Select Balance (no JnJ income or internal tnsf)')
+a.plot(X, Y, 'o', label='fit region')
+a.plot(X, p(X), '--', label='fit')
+a.plot(checking_df.days, checking_df.test_balance, '*', label='Test Balance')
+#a.plot(X, Yhat, '-', label='Select Balance barf')
+a.legend()
 
