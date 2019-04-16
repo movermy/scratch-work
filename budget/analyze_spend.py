0d046569fa4dd28f1774100ef837a15a4b159922
MarkOvermyer <movermye@its.jnj.com>
2019-04-16 13:30:11 -0400

    Half way through making analyze_spend.py Object Based. Dataframes are created, and used to make plots. Have not integrated line fittting.
    Signed-off-by: MarkOvermyer <movermye@its.jnj.com>

--- budget/analyze_spend.py
+++ budget/analyze_spend.py
@@ -14,67 +14,90 @@ import numpy as np
 plt.close('all')
 
 
-def chase_csv_import(path):
-    # When pointed to a directory of chase bank csv export, this 
-    # function creates a DataFrame with all the content. Because
-    # duplicates are droped from combo_df, you should be able to 
-    # drop csv exports in th efolder willy nilly, though this is
-    # not tested
-    csv_name_list = os.listdir(path)
-    df_list = []
+class SpendAnalysis():
     
-    for csv_name in csv_name_list:
-        df = pd.read_csv(os.path.join(path, csv_name))
-        df_list.append(df)
+    def __init__(self):
+        ''' Create and format a Dataframe for chase credit card'''
+        self.card_df = self.chase_csv_import("card-data")
+        self.card_df['source'] = 'card'
+        self.card_df['Date'] =pd.to_datetime(self.card_df['Post Date'])
         
-    combo_df = pd.concat(df_list, ignore_index=True)
-    combo_df.drop_duplicates(inplace=True)
-    combo_df['no_num_description'] = combo_df.Description.str.replace('\d+', '')
-    
-    return combo_df
-  
-
-
- 
-''' Create and format a Dataframe for chase credit card'''
-card_df = chase_csv_import("card-data")
-card_df['source'] = 'card'
-card_df['Date'] =pd.to_datetime(card_df['Post Date'])
-
-''' Create and format a Dataframe for chase checking. This Dataframe is used
-    to estimate cash flow out, so a balance column is created that has been
-    corrected for anomalies'''
-checking_df = chase_csv_import("checking-data")
-checking_df['source'] = 'checking'
-checking_df['Date'] =pd.to_datetime(checking_df['Posting Date'])
-checking_df['epoch'] = (checking_df['Date'] - dt.datetime(1970,1,1)).dt.total_seconds()
-
-checking_df['days'] = (checking_df['epoch'] - checking_df['epoch'].min())/(3600 * 24)
-
-checking_df['epoch'] = checking_df['epoch'] - checking_df['epoch'].min()
-
-checking_df.sort_values(by=['Date'], inplace=True)
-checking_df['test_balance'] = checking_df.Amount.cumsum() + checking_df.Balance.values[0] + 2300
-select_crit = ((~checking_df.Description.str.contains('J&J')) &\
-               (~checking_df.Description.str.contains('Wealthfront')) &\
-               (~checking_df.Description.str.contains('Apex Clearing')) &\
-               (~checking_df.Description.str.contains('ATM CHECK DEPOSIT 04/19 9019 PLAINFIELD RD BLUE ASH OH')) &\
-               (~checking_df.Description.str.contains('Online Transfer to SAV')) &\
-               (~checking_df.Description.str.contains('TRANSFER TO SAV'))) 
-checking_df['select_balance'] = checking_df.where(select_crit).Amount.cumsum() + checking_df.Balance.values[0]
-
-
-
-savings_df = chase_csv_import("savings-data")
-savings_df['source'] = 'savings'
-savings_df['Date'] =pd.to_datetime(savings_df['Posting Date'])
-
-# create and format the main df
-df = pd.concat([card_df, checking_df, savings_df], 
+        ''' Create and format a Dataframe for chase checking. This Dataframe is used
+        to estimate cash flow out, so a balance column is created that has been
+        corrected for anomalies'''
+        self.checking_df = self.chase_csv_import("checking-data")
+        self.checking_df['source'] = 'checking'
+        self.checking_df['Date'] =pd.to_datetime(self.checking_df['Posting Date'])
+        self.checking_df['epoch'] = (self.checking_df['Date'] - dt.datetime(1970,1,1)).dt.total_seconds()
+        
+        self.checking_df['days'] = (self.checking_df['epoch'] - self.checking_df['epoch'].min())/(3600 * 24)
+        
+        self.checking_df['epoch'] = self.checking_df['epoch'] - self.checking_df['epoch'].min()
+        
+        self.checking_df.sort_values(by=['Date'], inplace=True)
+        self.checking_df['test_balance'] = self.checking_df.Amount.cumsum() + self.checking_df.Balance.values[0] + 2300
+        select_crit = ((~self.checking_df.Description.str.contains('J&J')) &\
+                       (~self.checking_df.Description.str.contains('Wealthfront')) &\
+                       (~self.checking_df.Description.str.contains('Apex Clearing')) &\
+                       (~self.checking_df.Description.str.contains('ATM CHECK DEPOSIT 04/19 9019 PLAINFIELD RD BLUE ASH OH')) &\
+                       (~self.checking_df.Description.str.contains('Online Transfer to SAV')) &\
+                       (~self.checking_df.Description.str.contains('TRANSFER TO SAV'))) 
+        self.checking_df['select_balance'] = self.checking_df.where(select_crit).Amount.cumsum() + self.checking_df.Balance.values[0]
+        
+        ''' Create and formate Dataframe for Chase savings '''
+        self.savings_df = self.chase_csv_import("savings-data")
+        self.savings_df['source'] = 'savings'
+        self.savings_df['Date'] =pd.to_datetime(self.savings_df['Posting Date'])
+        
+        # create and format the main df
+        self.all_chase_df = pd.concat([self.card_df, self.checking_df, self.savings_df], 
                ignore_index=True, sort=True)
 
-df.sort_values(by=['Date'], inplace=True)
+        self.all_chase_df.sort_values(by=['Date'], inplace=True)
+        
 
+    def chase_csv_import(self, path):
+        # When pointed to a directory of chase bank csv export, this 
+        # function creates a DataFrame with all the content. Because
+        # duplicates are droped from combo_df, you should be able to 
+        # drop csv exports in th efolder willy nilly, though this is
+        # not tested
+        csv_name_list = os.listdir(path)
+        df_list = []
+        
+        for csv_name in csv_name_list:
+            df = pd.read_csv(os.path.join(path, csv_name))
+            df_list.append(df)
+            
+        combo_df = pd.concat(df_list, ignore_index=True)
+        combo_df.drop_duplicates(inplace=True)
+        combo_df['no_num_description'] = combo_df.Description.str.replace('\d+', '')
+        
+        return combo_df
+  
+    def raw_chase_plot(self):
+        # plot the raw balances for chase related accounts 
+        
+        f,a = plt.subplots(1,1)
+        f.suptitle('Summary of Chase accounts - raw')
+        
+        crit = self.all_chase_df.source.str.contains("checking")
+        a.plot(self.all_chase_df.loc[crit].Date, 
+               self.all_chase_df.loc[crit].Balance, 
+               label='Checking Balance')
+        
+        crit = self.all_chase_df.source.str.contains("savings")
+        a.plot(self.all_chase_df.loc[crit].Date, 
+               self.all_chase_df.loc[crit].Balance, 
+               label='Savings Balance')
+        
+        plt.plot(self.all_chase_df.loc[crit].Date, 
+                 self.all_chase_df.loc[crit].Balance, 'g*')
+
+
+
+
+'''
 # high level stats
 crit = (df['no_num_description'].str.lower().str.contains('itunes') &\
         df['no_num_description'].str.lower().str.contains('bill'))
@@ -83,16 +106,7 @@ itunes_spend = df.loc[crit].Amount.sum()
 crit = (df['no_num_description'].str.lower().str.contains('j&j'))
 mark = df.loc[crit]
 
-# plot raw balences 
-f,a = plt.subplots(1,1)
-f.suptitle('Summary of Chase accounts - raw')
-crit = df.source.str.contains("checking")
-a.plot(df.loc[crit].Date, df.loc[crit].Balance, label='Checking Balance')
-crit = df.source.str.contains("savings")
 
-a.plot(df.loc[crit].Date, df.loc[crit].Balance, label='Savings Balance')
-
-plt.plot(df.loc[crit].Date, df.loc[crit].Balance, 'g*')
 
 f,a = plt.subplots(1,1)
 start_time = '2017-05-18'
@@ -144,4 +158,6 @@ a.plot(checking_df.days, checking_df.test_balance, '*', label='Test Balance')
 #a.plot(X, Yhat, '-', label='Select Balance barf')
 a.legend()
 
-#https://towardsdatascience.com/time-series-analysis-in-python-an-introduction-70d5a5b1d52a
+
+#if __name__ == '__main__':
+'''
