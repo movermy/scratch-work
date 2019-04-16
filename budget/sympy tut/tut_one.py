bd77af772fbb47ba919692c1faef55f1169de308
MarkOvermyer <movermye@its.jnj.com>
2019-04-15 23:00:55 -0400

    budget: basic sympy tut with offsetted exponential functions

--- budget/sympy tut/tut_one.py
+++ budget/sympy tut/tut_one.py
@@ -9,11 +9,27 @@ from sympy import symbols
 from sympy import expand, factor
 from sympy.plotting import plot
 from sympy.functions.special.delta_functions import Heaviside
+from mpmath import e
+import matplotlib.pyplot as plt
 
-x, y = symbols('x y')
+plt.close('all')
 
-expr = x + 2*y
 
-my_line = 1 * (x - 5) * Heaviside(x - 5)
 
-p1 = plot(my_line)
+x, y, t = symbols('x y t')
+P = 409
+r = .07
+marks_earnings = P * e ** (r * (t-0)) * Heaviside(t-0)
+P = 100
+r = .03
+karens_earnings = P * e ** (r * (t-3)) * Heaviside(t-3)
+total_earnings = marks_earnings + karens_earnings
+
+offset = x - 5
+my_line = 1 * (offset) * Heaviside(offset)
+
+
+
+
+plot(total_earnings)
+plot(my_line)
