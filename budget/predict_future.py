d148c5ef22cd69ce488355e05e59687bfb7d8ca2
MarkOvermyer <movermye@its.jnj.com>
2019-05-04 10:29:34 -0400

    added future values of hsa, ira, and roth

--- budget/predict_future.py
+++ budget/predict_future.py
@@ -35,25 +35,37 @@ end_date = (date(2020,5,3))
 rng = pd.date_range(start_date, end_date, freq='MS')
 rng.name = "Payment_Date"
 
-df = pd.DataFrame(index=rng, columns=['Wealthfront', 'Chase'], dtype='float')
+cols = ['Wealthfront', 'Chase', 'IRA_principal', 'roth_principal', 'hsa_principal']
+df = pd.DataFrame(index=rng, columns=cols, dtype='float')
 df.reset_index(inplace=True)
 df.index += 1
 df.index.name = "Period"
 
-annual_interest_rate = c.wealthfront_rate
 compounds_per_year = 12
-payment = -0
 df.Wealthfront = np.fv(c.wealthfront_rate/compounds_per_year,
                        df.index,
-                       payment,
+                       -0,
                        -c.wealthfront_principal)
 
-#df.plot(x='Payment_Date', y='Wealthfront')
-plt.show(block=False)
+df.IRA_principal = np.fv(c.IRA_rate/compounds_per_year,
+                         df.index,
+                         -c.bi_weekly_IRA_contribution * 2,
+                         -c.IRA_principal)
+
+df.roth_principal = np.fv(c.roth_rate/compounds_per_year,
+                          df.index,
+                          -c.bi_weekly_roth_contribution * 2,
+                          -c.roth_principal)
+
+df.hsa_principal = np.fv(c.HSA_rate/compounds_per_year,
+                         df.index,
+                         -c.bi_weekly_hsa_contribution * 2,
+                         -c.HSA_principal)
+
 
 # build chase cash flow
 pre_tax_deductions = (c.bi_weekly_IRA_contribution +
-                      c.bi_weekly_HSA +
+                      c.bi_weekly_hsa_contribution +
                       c.bi_weekly_medical) * 2
 
 monthly_pre_tax_income = (c.daily_pre_tax_mark * 30 - pre_tax_deductions)
@@ -64,11 +76,10 @@ post_tax_deductions = ((c.bi_weekly_roth_contribution +
 monthly_post_tax_income = monthly_pre_tax_income * (1 - c.tax_rate) - post_tax_deductions
 
 df.Chase = (monthly_post_tax_income + sa.spend_dollars_per_day * 30) * df.index + c.chase_start_amount
-#df.plot(x='Payment_Date', y='Chase')
 
 #OLeary mortgage
-start_date = (date(2010,4,3))
-end_date = (date(2020,5,3))
+start_date = (date(2010, 4, 3))
+end_date = (date(2020, 5, 3))
 rng = pd.date_range(start_date, end_date, freq='MS')
 rng.name = "Payment_Date"
 
@@ -106,5 +117,6 @@ print(oldf.head())
 
 # combine mortgage df with main df
 df = pd.merge(df, oldf, on=['Payment_Date']) #TODO there is probably a more efficient way to do this
+print(df.round(2).head())
 plt.show()
 
