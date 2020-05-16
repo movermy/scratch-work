d7f88ab182a79571d087aa13db043b491caba795
MarkOvermyer <movermye@its.jnj.com>
2020-05-16 19:26:37 -0400

    sound_test.py now uses Sound object, making Sound.get_length available

--- joystick/sound_test.py
+++ joystick/sound_test.py
@@ -3,20 +3,9 @@ import time         #calling for time to provide delays in program
 
 from joystick_utilities import get_random_audio_path
 
-# Starting the mixer
+
 mixer.init()
 
-# Loading the song
-mixer.music.load(get_random_audio_path())
-
-# Setting the volume
-mixer.music.set_volume(0.7)
-
-# Start playing the song
-mixer.music.play(-1)
-time.sleep(3)
-
-'''
-if __name__ == "__main__":
-    print(get_audio_directory_path())
-'''
+random_sound = mixer.Sound(get_random_audio_path())
+random_sound.play()
+time.sleep(random_sound.get_length())
