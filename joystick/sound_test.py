550b34968f30a0a46ba1479cfca010097eec32e3
mark_on_pi <movermy@gmail.com>
2020-05-20 08:46:31 -0400

    basic button-to-sound functionallity working on raspi. Needed to make new Python env with 3.7. Should do this on Windows to troubleshoot no play. Note: sound effects queue for each button press. Don't do this

--- joystick/sound_test.py
+++ joystick/sound_test.py
@@ -1,11 +1,15 @@
 from pygame import mixer #calling for pygame mixer to play audio files
+import pygame
 import time         #calling for time to provide delays in program
 
 from joystick_utilities import get_random_audio_path
 
+#pygame.init()  #adding this causes intermittent errors with GIL
 
 mixer.init()
 
 random_sound = mixer.Sound(get_random_audio_path())
 random_sound.play()
 time.sleep(random_sound.get_length())
+
+#pygame.quit() #adding this causes intermittent errors with GIL
